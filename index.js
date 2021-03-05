26a8b32b7b2517de086a46859a9c0b21f972123c
oleg90990 <oleg909902@yandex.ru>
2021-03-06 02:18:29 +0300

    fx

--- index.js
+++ index.js
@@ -4,39 +4,13 @@ const express = require('express')
 const http = require('http').createServer(app)
 const path = require('path')
 const { v4: uuidv4 } = require('uuid');
+const mapPositions = JSON.parse(fs.readFileSync('mapPositions.json'));
 const io = require('socket.io')(http, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"]
   }
 })
-
-var timeBonus = 10000;
-var mapPositions = fs.readFileSync('mapPositions.json');
-mapPositions = JSON.parse(mapPositions);
-
-var newBonus = {
-  id: '',
-  x: 0,
-  y: 0,
-  model: 'Heart',
-  value: 30
-};
-
-setInterval(function (argument) {
-  const pos = randomPosition()  
-  newBonus = {
-    id: uuidv4(),
-    x: pos.x,
-    y: pos.y,
-    model: 'Heart',
-    value: 30
-  }
-}, timeBonus)
-
-function randomPosition() {
-  return mapPositions[Math.floor(Math.random() * mapPositions.length)];
-}
 
 app.use("/css", express.static(path.join(__dirname, 'game/dist/css')))
 app.use("/js", express.static(path.join(__dirname, 'game/dist/js')))
@@ -66,12 +40,23 @@ io.on('connection', (socket) => {
   socket.on("disconnect", () => {
     socket.broadcast.emit('disconectitem', socket.id)
   });
-
-  setInterval(function (argument) {
-    socket.emit('bonus', newBonus)
-  }, timeBonus)
 })
+
+setInterval(function (argument) {
+  const pos = mapPositions[Math.floor(Math.random() * mapPositions.length)];
+
+  socket.emit('bonus', {
+    id: uuidv4(),
+    x: pos.x,
+    y: pos.y,
+    model: 'Heart',
+    value: 30
+  })
+}, 10000)
 
 http.listen(3000, () => {
   console.log('listening on *:3000')
 })
+
+
+
